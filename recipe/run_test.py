f6aea43165bbc98a73f69c63f51a67ab661bb58e
Bo <BoPeng@users.noreply.github.com>
2021-09-30 16:02:20 -0500

    Update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import time
 
 from sos_notebook.test_utils import sos_kernel
 from ipykernel.tests.utils import execute, wait_for_idle, assemble_output
@@ -11,10 +12,12 @@ class TestSoSKernel(unittest.TestCase):
             stdout, stderr = assemble_output(kc.get_iopub_msg)
             self.assertEqual(stdout.strip(), '', f'Stdout is not empty, "{stdout}" received')
             self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
+            time.sleep(30)
             execute(kc=kc, code='%use Julia\n%get a\nprint(a)')
-            #stdout, stderr = assemble_output(kc.get_iopub_msg)
-            #self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
-            #self.assertEqual(stdout.splitlines()[-1].strip(), '1', f'Stdout should be 1, "{stdout}" received')
+            time.sleep(10)
+            stdout, stderr = assemble_output(kc.get_iopub_msg)
+            self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
+            self.assertEqual(stdout.splitlines()[-1].strip(), '1', f'Stdout should be 1, "{stdout}" received')
 
 if __name__ == '__main__':
     unittest.main()
