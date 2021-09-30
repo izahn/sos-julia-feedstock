8255ccad193d4f36a5214003757c5cd8eaf3dfa8
Bo <BoPeng@users.noreply.github.com>
2021-09-30 14:40:43 -0500

    Update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import time
 
 from sos_notebook.test_utils import sos_kernel
 from ipykernel.tests.utils import execute, wait_for_idle, assemble_output
@@ -12,6 +13,7 @@ class TestSoSKernel(unittest.TestCase):
             self.assertEqual(stdout.strip(), '', f'Stdout is not empty, "{stdout}" received')
             self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
             execute(kc=kc, code='%use Julia\n%get a\nprint(a)')
+            time.sleep(10)
             stdout, stderr = assemble_output(kc.get_iopub_msg)
             self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
             self.assertEqual(stdout.splitlines()[-1].strip(), '1', f'Stdout should be 1, "{stdout}" received')
